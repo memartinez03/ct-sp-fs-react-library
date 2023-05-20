8ba850ad5f65a1bf58e7b3d810da7d0e7b718020
memartinez03 <106893983+memartinez03@users.noreply.github.com>
2023-05-20 10:34:32 -0700

    Delete Documents/coding-temple-self-paced/React/ct-sp-fs-react-library-update_v18/ct-sp-fs-react-library-update-v18 directory

--- Documents/coding-temple-self-paced/React/ct-sp-fs-react-library-update_v18/ct-sp-fs-react-library-update-v18/src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-// Store is like a mini temporary in-the-middle database
-
-import { configureStore } from "@reduxjs/toolkit";
-import { reducer } from './slices/RootSlice'
-
-export const store = configureStore({
-    reducer,
-    devTools: true,
-})
